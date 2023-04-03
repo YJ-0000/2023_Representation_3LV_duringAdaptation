12147a82cfc25f6c3a3988cd59fd9631c2193d92
YJ-0000 <ys1j13@gmail.com>
2023-04-03 17:55:51 +0900

    modify fmri analysis codes

--- Code_fMRI_analysis/Step2_fmri04_1stLevelAnalysis.m
+++ Code_fMRI_analysis/Step2_fmri04_1stLevelAnalysis.m
@@ -20,9 +20,9 @@ pathBehav = pwd;
 load('beh_results.mat');
 
 cd(pathDeriv);
-mkdir('spm-mvpa'); cd('spm-mvpa');
+mkdir('spm-bold'); cd('spm-bold');
 pathBOLD = pwd;
-mkdir('glm-1stlevel'); cd('glm-1stlevel');
+mkdir('bold-1stlevel'); cd('bold-1stlevel');
 path1LV = pwd;
 
 
@@ -35,6 +35,13 @@ disp('init');
 spm_jobman('initcfg');
 
 directions = {'rightward', 'upward', 'leftward', 'downward'};
+
+mirror_contrast_vector = [];
+rot_pos90_contrast_vector = [];
+rot_neg90_contrast_vector = [];
+session_start_contrast_vector = [];
+session_change_contrast_vector = [];
+score_contrast_vector = [];
    
 for nsub = 1:length(prepSubjFolders)
     % make subject directory
@@ -43,7 +50,7 @@ for nsub = 1:length(prepSubjFolders)
     % ready func images
     frame = 0;
     cd([prepSubjFolders(nsub).folder filesep prepSubjFolders(nsub).name filesep 'func']);
-    funcFilePath = conn_dir('s2au*bold.nii');
+    funcFilePath = conn_dir('s8wau*bold.nii');
     ninfo = niftiinfo(funcFilePath);
     clear scanFileList
     for ff = 1:ninfo.ImageSize(4)
@@ -79,78 +86,108 @@ for nsub = 1:length(prepSubjFolders)
     % condition (convoluted with hrf)
     
     regressNum = 0;
+    
+    session_change_contrast_vector = zeros(15,1);
     for nses = 1:15
-        temp_onset = onset(session_number == nses);
-        temp_duration = duration(session_number == nses);
-        temp_ses_type = unique(session_type(session_number == nses)); temp_ses_type = temp_ses_type{1};
-        temp_stimulus_direction = eventtable.stimulus_direction(session_number == nses);
-        temp_is_tooExplored = squeeze(is_tooExplored_trial(nsub,2,nses,:));
-        
-        % regressor for session start
-        regressNum = regressNum + 1;
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = ['session_start_' temp_ses_type];
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = temp_onset(1);
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = temp_duration(1);
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
-        
-        % regressor for erasing
-        regressNum = regressNum + 1;
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = 'first trial (excluded)';
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = temp_onset(2);
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = temp_duration(2);
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
-        
-        temp_erasing_onset = temp_onset(3:18);
-        temp_erasing_duration = temp_duration(3:18);
-        temp_stimulus_direction = temp_stimulus_direction(3:18);
-        temp_is_tooExplored(1) = [];
-        
-        for n_type = 1:4
-            regressNum = regressNum + 1;
-            temp_idx = strcmp(temp_stimulus_direction,directions{n_type});
-            temp_idx(temp_is_tooExplored) = false;
-            if ~any(temp_idx)
-                error('No valid trial!!! Something went wrong!!!');
+        if nses > 1
+            temp_prev_ses_type = unique(session_type(session_number == (nses-1))); temp_prev_ses_type = temp_prev_ses_type{1};
+            temp_ses_type = unique(session_type(session_number == nses)); temp_ses_type = temp_ses_type{1};
+            if strcmp(temp_prev_ses_type,temp_ses_type)
+                session_change_contrast_vector(nses) = -1;
+            else
+                session_change_contrast_vector(nses) = 1;
             end
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = ['erasing_' temp_ses_type '_' directions{n_type}];
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = temp_erasing_onset(temp_idx);
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = temp_erasing_duration(temp_idx);
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
         end
-        
-        for temp_i = find(temp_is_tooExplored)'
-            regressNum = regressNum + 1;
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = ['erasing_too_explored_' temp_ses_type '_' directions{n_type}];
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = temp_erasing_onset(temp_i);
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = temp_erasing_duration(temp_i);
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
-            matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
-        end
-        
-        regressNum = regressNum + 1;
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = 'score';
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = temp_onset(19);
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = temp_duration(19);
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
-        
-        regressNum = regressNum + 1;
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = 'session end';
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = temp_onset(20);
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = temp_duration(20);
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
-        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
-
     end
+    regressNum = regressNum + 1;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = 'session_start';
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = onset(strcmp(trial_type,'start_session'));
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = duration(strcmp(trial_type,'start_session'));
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod.name = 'task switch';
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod.param = session_change_contrast_vector;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod.poly = 1;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
+    
+    is_tooExplored = squeeze(is_tooExplored_trial(nsub,2,:,:));
+    is_tooExplored = is_tooExplored';
+    is_tooExplored = is_tooExplored(:);
+    
+    ses_name = 'rotation+90';
+    temp_onset = onset(strcmp(trial_type,'erasing') & strcmp(session_type,ses_name));
+    temp_duration = duration(strcmp(trial_type,'erasing') & strcmp(session_type,ses_name));
+    ses_idx = strcmp(session_type(strcmp(trial_type,'erasing')),ses_name);
+    temp_onset(is_tooExplored(ses_idx)) = [];
+    temp_duration(is_tooExplored(ses_idx)) = [];
+    
+    regressNum = regressNum + 1;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = 'rotation+90';
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = temp_onset;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = temp_duration;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
+    
+    ses_name = 'rotation-90';
+    temp_onset = onset(strcmp(trial_type,'erasing') & strcmp(session_type,ses_name));
+    temp_duration = duration(strcmp(trial_type,'erasing') & strcmp(session_type,ses_name));
+    ses_idx = strcmp(session_type(strcmp(trial_type,'erasing')),ses_name);
+    temp_onset(is_tooExplored(ses_idx)) = [];
+    temp_duration(is_tooExplored(ses_idx)) = [];
+    
+    regressNum = regressNum + 1;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = 'rotation-90';
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = temp_onset;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = temp_duration;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
+    
+    ses_name = 'mirror';
+    temp_onset = onset(strcmp(trial_type,'erasing') & strcmp(session_type,ses_name));
+    temp_duration = duration(strcmp(trial_type,'erasing') & strcmp(session_type,ses_name));
+    ses_idx = strcmp(session_type(strcmp(trial_type,'erasing')),ses_name);
+    temp_onset(is_tooExplored(ses_idx)) = [];
+    temp_duration(is_tooExplored(ses_idx)) = [];
+    
+    regressNum = regressNum + 1;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = 'mirror';
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = temp_onset;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = temp_duration;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
+    
+    regressNum = regressNum + 1;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = 'score';
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = onset(strcmp(trial_type,'score'));
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = duration(strcmp(trial_type,'score'));
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
+    
+    regressNum = regressNum + 1;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = 'session end';
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = onset(strcmp(trial_type,'end_session'));
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = duration(strcmp(trial_type,'end_session'));
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
+    matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
+
+    
+    temp_onset = onset(strcmp(trial_type,'erasing'));
+    temp_duration = duration(strcmp(trial_type,'erasing'));
+    
+    for temp_i = find(is_tooExplored)'
+        regressNum = regressNum + 1;
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).name = 'erasing_too_explored_';
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).onset = temp_onset(temp_i);
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).duration = temp_duration(temp_i);
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).tmod = 0;
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).pmod = struct('name', {}, 'param', {}, 'poly', {});
+        matlabbatch{1}.spm.stats.fmri_spec.sess.cond(regressNum).orth = 1;
+    end
+    
 
     matlabbatch{1}.spm.stats.fmri_spec.sess.multi = {''};
     % regressors for covariate
@@ -183,6 +220,28 @@ for nsub = 1:length(prepSubjFolders)
     matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('fMRI model specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
     matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
     matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+    % contrast manager
+    matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+    matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'erasing rotation+90';
+    matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = [0, 0, 1];
+    matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+    matlabbatch{3}.spm.stats.con.consess{2}.tcon.name = 'erasing rotation-90';
+    matlabbatch{3}.spm.stats.con.consess{2}.tcon.weights = [0, 0, 0, 1];
+    matlabbatch{3}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+    matlabbatch{3}.spm.stats.con.consess{3}.tcon.name = 'erasing mirror';
+    matlabbatch{3}.spm.stats.con.consess{3}.tcon.weights = [0, 0, 0, 0, 1];
+    matlabbatch{3}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+    matlabbatch{3}.spm.stats.con.consess{4}.tcon.name = 'session start';
+    matlabbatch{3}.spm.stats.con.consess{4}.tcon.weights = [1];
+    matlabbatch{3}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
+    matlabbatch{3}.spm.stats.con.consess{5}.tcon.name = 'session change';
+    matlabbatch{3}.spm.stats.con.consess{5}.tcon.weights = [0, 1];
+    matlabbatch{3}.spm.stats.con.consess{5}.tcon.sessrep = 'none';
+    matlabbatch{3}.spm.stats.con.consess{6}.tcon.name = 'score';
+    matlabbatch{3}.spm.stats.con.consess{6}.tcon.weights = [0, 0, 0, 0, 0, 1];
+    matlabbatch{3}.spm.stats.con.consess{6}.tcon.sessrep = 'none';
+    matlabbatch{3}.spm.stats.con.delete = 0;
+    
 
     disp('run');
     spm_jobman('run', matlabbatch);
